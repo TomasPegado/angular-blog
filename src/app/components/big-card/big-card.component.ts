caf899bd5690ea8f3defa0e221995dc99183b2bb
TomasPegado <lenzitomas00@gmail.com>
2024-01-13 20:02:47 -0300

    componentes dinamicos

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
+  @Input()
+  photoCover: string = '';
 
+  @Input()
+  cardTitle: string = 'DRAGÃO EM CAMPO DE BATALHA';
+
+  @Input()
+  cardDescription: string =
+    'O dragão entrou no campo de batalha e virou o jogo os verdes';
 }
