f7928501fc5edb9521ed72955a0485c52979ecb3
TomasPegado <lenzitomas00@gmail.com>
2024-01-23 14:56:31 -0300

    content Page Dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,17 @@
 export const dataFake = [
   {
     id: '1',
-    photo: 'aaa',
-    title: 'Saiu novo filme',
-    description: 'ola',
+    photoCover:
+      'https://ew.com/thmb/_eeSW0rNytiU5CMQM6c1IA_IXLE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/wil-johnson-theo-nate-steve-toussaint-solly-mccleod-house-of-the-dragon-090222-e11ead875b3f439f8c7ebca283deffd6.jpg',
+    title: 'Dragão no Campo de Batalha',
+    description:
+      'Um Dragão entrou no campo de batalha e virou o jogo para o time dos vermelhos',
   },
   {
-    id: 2,
-    photo: 'aaa',
-    title: 'Saiu novo filme',
-    description: 'ola',
+    id: '2',
+    photoCover:
+      'https://ca-times.brightspotcdn.com/dims4/default/2553d57/2147483647/strip/true/crop/640x427+0+0/resize/1200x801!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F7f%2F81%2F5aa6f1374022a96c1af672e64a38%2Fmatt-smith-0.jpg',
+    title: 'Deamon em Torneio',
+    description: 'Deamon vence seus oponentes em torneio em nome do Rei',
   },
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -8,19 +8,25 @@ import { dataFake } from '../../data/dataFake';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent {
-  photoCover: string =
-    'https://ew.com/thmb/_eeSW0rNytiU5CMQM6c1IA_IXLE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/wil-johnson-theo-nate-steve-toussaint-solly-mccleod-house-of-the-dragon-090222-e11ead875b3f439f8c7ebca283deffd6.jpg';
-  contentTitle: string = 'MINHA NOTICIA';
-  contentDescription: string = 'Olá, Mundo!';
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
   private id: string | null = '0';
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+
+    this.setValuesToComponent(this.id);
   }
 
-  setValuesToComponent(id: string) {
-    const result = dataFake.filter((articles) => articles.id == id);
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((articles) => articles.id == id)[0];
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
+    this.id = result.id;
   }
 }
